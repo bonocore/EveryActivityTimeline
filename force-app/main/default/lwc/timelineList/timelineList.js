05e45059532076747512d23e11121b003e076194
giuseppe bonocore <giuseppe.bonocore@gmail.com>
2022-08-19 18:12:08 +0200

    Created custom object and wired to select object instances

--- force-app/main/default/lwc/timelineList/timelineList.js
+++ force-app/main/default/lwc/timelineList/timelineList.js
@@ -1,8 +1,9 @@
-import { LightningElement } from 'lwc';
-import { activities } from 'c/timelineData';
-
+import { LightningElement,api,wire } from 'lwc';
+import getAllTimelineActivities from '@salesforce/apex/GB_Timeline_Activity_Controller.getAllTimelineActivities';
+import getLeadTimelineActivities from '@salesforce/apex/GB_Timeline_Activity_Controller.getLeadTimelineActivities';
 export default class TimelineList extends LightningElement {
-    activities = activities;
-
-
+ 
+    @api recordId;
+    //@wire(getAllTimelineActivities) activities;
+	@wire(getLeadTimelineActivities, {leadId: '$recordId'}) activities;
 }
