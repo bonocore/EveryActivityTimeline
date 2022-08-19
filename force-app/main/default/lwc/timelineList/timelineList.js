abd5f0c27ae5ff1e6b05839161d1070a04a45bff
giuseppe bonocore <giuseppe.bonocore@gmail.com>
2022-08-19 12:47:37 +0200

    First Commit

---
+++ force-app/main/default/lwc/timelineList/timelineList.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import { activities } from 'c/timelineData';
+
+export default class TimelineList extends LightningElement {
+    activities = activities;
+
+
+}
