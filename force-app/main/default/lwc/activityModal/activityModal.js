ff69b3eeb93ace5cdf5ab1ddfa004965cfcc5ddd
giuseppe bonocore <giuseppe.bonocore@gmail.com>
2022-08-25 11:16:46 +0200

    Added "New activity" button. Refresh working

---
+++ force-app/main/default/lwc/activityModal/activityModal.js
@@ -0,0 +1,12 @@
+import { LightningElement,api  } from 'lwc';
+
+export default class ActivityModal extends LightningElement {
+
+    @api isActivityModalOpen = false;
+    @api leadId;
+   
+    handleClose() {
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+
+}
--- force-app/main/default/lwc/timelineList/timelineList.js
+++ force-app/main/default/lwc/timelineList/timelineList.js
@@ -1,9 +1,42 @@
-import { LightningElement,api,wire } from 'lwc';
+import { LightningElement,api,wire,track } from 'lwc';
 import getAllTimelineActivities from '@salesforce/apex/GB_Timeline_Activity_Controller.getAllTimelineActivities';
 import getLeadTimelineActivities from '@salesforce/apex/GB_Timeline_Activity_Controller.getLeadTimelineActivities';
+import { refreshApex } from '@salesforce/apex';
+
+
 export default class TimelineList extends LightningElement {
- 
+
     @api recordId;
-    //@wire(getAllTimelineActivities) activities;
-	@wire(getLeadTimelineActivities, {leadId: '$recordId'}) activities;
+
+    @track isActivityModalOpen = false;
+
+    @track activities;
+
+    wiredActivitiesResult; //useful for forcing refresh
+
+    @wire(getLeadTimelineActivities, {leadId: '$recordId'}) 
+    wiredActivities(result){
+        this.wiredActivitiesResult=result;
+
+        if (result.data) {
+          this.activities = result.data;
+          this.error = undefined;
+        } else if (result.error) {
+          this.error = result.error;
+          console.log("Error "+ JSON.stringify(result.error));
+          this.activities = undefined;
+        }
+      }
+
+    
+    openActivityModal() {
+        this.isActivityModalOpen = true;
+    }
+    closeActivityModal() {
+        this.isActivityModalOpen = false;
+    }
+    submitActivityModal() {
+        this.isActivityModalOpen = false;
+        return refreshApex(this.wiredActivitiesResult);
+    }
 }
